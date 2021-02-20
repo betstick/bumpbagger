4cef369778be67f0d4db4d706e4deb54070b9ce9
betstick <61669265+betstick@users.noreply.github.com>
2021-02-20 02:40:57 -0500

    added config file and did better logging

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-import urllib
-import os
+import urllib, configparser, os, time
 from urllib.request import urlretrieve
-import time
+
+config = configparser.ConfigParser()
+config.read('config')
+downloadlocation = config['paths']['downloadlocation']
 
 nums = range(0,8749) #this is roughly how many videos there are URLs for! its not exact. edit it to your needs
 
-downloadlocation = "/home/yourhomefolder/downloadsmaybe" #edit this to where you want the files! mind you /'s and \'s
-
-#scrape the url name
+#scrape the url name from the video page title html
 def findTitle(url):
     webpage = urllib.request.urlopen(url).read()
     title = str(webpage).split('<title>')[1].split('</title>')[0]
@@ -16,17 +16,18 @@ def findTitle(url):
 #edits out these chars to prevent name issues on various operating systems/toochains
 charlist = ['\\','/',".","!","@","#","$","%","^","&","*","(",")",";",":","[","]","{","}","|","'",'"',",","<",">","?","`","~","="]
 
-i = 0
+i = 0 #index to keep track and to name videos
+
 for num in nums:
 	title = findTitle("https://www.bumpworthy.com/bumps/"+str(num)).replace(" | BumpWorthy.com - adult swim bumps","").replace(" ","_")
 	for char in charlist:
 		title = title.replace(char,"")
 
-	path = +title+"."+str(num)+".mp4"
-	url = downloadlocation+"https://www.bumpworthy.com/download/video/"+str(num)
+	path = downloadlocation+title+"."+str(num)+".mp4"
+	url = "https://www.bumpworthy.com/download/video/"+str(num)
 	if (os.path.exists(path) != True):
 		print(path + " " url)
 		urlretrieve(url, filename=path)
 	else:
-		print("file already downloaded :P :"+str(i))
+		print("File already downloaded. Number: "+str(i))
 	i = i + 1
